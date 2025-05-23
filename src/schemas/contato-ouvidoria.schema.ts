fbf6b10e20aa640571869c7a06760a04ca302b39
unknown <luis.portugal@sofintech.com.br>
2025-05-22 23:48:58 -0300

    validacoes

--- src/schemas/contato-ouvidoria.schema.ts
+++ src/schemas/contato-ouvidoria.schema.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
+import { capitalizeWords, onlyNumbers } from '../utils/helpers';
 
 export const ContatoOuvidoriaSchema = z.object({
-  nome: z.string().optional(),
-  email: z.string().email("O campo E-mail inválido").optional(),
-  telefone: z.string().optional(),
-  mensagem: z.string({required_error: "O campo Mensagem é obrigatório"}),
+  nome: z.string().trim().transform(capitalizeWords).optional(),
+  email: z.string().email("O campo E-mail inválido").trim().optional(),
+  telefone: z.string().trim().transform(onlyNumbers).optional(),
+  mensagem: z.string({required_error: "O campo Mensagem é obrigatório"}).trim(),
 });
--- src/schemas/contato.schema.ts
+++ src/schemas/contato.schema.ts
@@ -1,11 +1,12 @@
 import { z } from 'zod';
+import { capitalizeWords, onlyNumbers } from '../utils/helpers';
 
 export const ContatoSchema = z
   .object({
-    nome: z.string({ required_error: "O campo Nome é obrigatório" }),
-    email: z.string().email("O campo E-mail inválido"),
-    telefone: z.string(),
-    mensagem: z.string({ required_error: "O campo Mensagem é obrigatório" }),
+    nome: z.string({ required_error: "O campo Nome é obrigatório" }).trim().transform(capitalizeWords),
+    email: z.string().trim().email("O campo E-mail inválido"),
+    telefone: z.string().trim().transform(onlyNumbers),
+    mensagem: z.string({ required_error: "O campo Mensagem é obrigatório" }).trim(),
   })
   .superRefine((data, ctx) => {
     if (!data.email && !data.telefone) {
