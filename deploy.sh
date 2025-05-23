1e9d023afea5fcacdec0d261e099627396007a93
unknown <luis.portugal@sofintech.com.br>
2025-05-22 23:51:23 -0300

    modificacoes no deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
+echo "baixando atualizações"
+git pull origin main
+
+echo "removendo versão antiga do docker"
 docker stop website-api
 docker container rm website-api
+
+echo "buildando versão nova"
 docker build -t website-api .
+
+echo "subindo versão nova"
 docker run -p 8080:8080 --name website-api -d website-api
