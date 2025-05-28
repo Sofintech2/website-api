d195b5cffb7a088107af6a7a96600c2830ceaa05
unknown <luis.portugal@sofintech.com.br>
2025-05-27 23:07:02 -0300

    ajuste schema

--- src/schemas/denuncia.schema.ts
+++ src/schemas/denuncia.schema.ts
@@ -2,16 +2,16 @@ import { z } from 'zod';
 import { capitalizeWords, onlyNumbers } from '../utils/helpers';
 
 export const DenunciaSchema = z.object({
-  email: z.string().email("O campo E-mail inválido").trim().optional(),
-  tipoReporte: z.enum(["Denúncia", "Dúvida", "Sugestão"], { required_error: "O campo Tipo de Reporte é obrigatório", message: "O campo Tipo de Reporte está inválido" }),
-  categoriaAssunto: z.string({required_error: 'O campo Categoria do Assunto é obrigatório'}).trim().transform(capitalizeWords),
-  politicaInterna: z.string({required_error: 'O campo Política interna é obrigatório'}).trim().transform(capitalizeWords),
+  email: z.string().trim(),
+  tipoReporte: z.string({required_error: "O campo Tipo de Reporte é obrigatório" }).trim(),
+  categoriaAssunto: z.string({ required_error: 'O campo Categoria do Assunto é obrigatório' }).trim().transform(capitalizeWords),
+  politicaInterna: z.string({ required_error: 'O campo Política interna é obrigatório' }).trim().transform(capitalizeWords),
   descricao: z.string({ required_error: "O campo Descrição do Incidente/Dúvida é obrigatório" }).trim(),
-  data: z.string({ required_error: "O campo Data do Incidente é obrigatório"}).datetime({local: true, message: "O campo Data do Incidente está inválido"}),
+  data: z.string({ required_error: "O campo Data do Incidente é obrigatório" }).trim(),
   local: z.string({ required_error: "O campo Local do Incidente é obrigatório" }).trim().transform(capitalizeWords),
-  medidaResolverProblema: z.string().trim().transform(capitalizeWords).optional(),
+  medidaResolverProblema: z.string().trim().transform(capitalizeWords),
   arquivos: z.array(z.object({
     originalName: z.string(),
     downloadUrl: z.string()
-})).optional()
+  })).optional()
 });
